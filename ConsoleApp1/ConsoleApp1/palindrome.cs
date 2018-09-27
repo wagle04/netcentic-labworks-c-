01e258c884bdcf248a6d2c8254c2d13a45a38656
wagle04 <waglemanish28@gmail.com>
2018-09-27 19:47:23 +0545

    9/27/2018

--- ConsoleApp1/ConsoleApp1/palindrome.cs
+++ ConsoleApp1/ConsoleApp1/palindrome.cs
@@ -10,8 +10,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string a;
             bool flag = false;
+            Console.WriteLine("Enter a string:");
+            string a = Console.ReadLine();
+            int l = a.Length;
+            for (int i = 0; i < l; i++)
+            {
+                if (a[i] == a[l - i - 1])
+                {
+                    flag = true;
+                }
+                else
+                {
+                    Console.WriteLine("Not a palindrome");
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag == true)
+            {
+                Console.WriteLine("Palindrome");
+            }
+
+            Console.ReadKey();
 
         }
     }
