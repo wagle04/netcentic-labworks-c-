c964a3e84865ab0c1e4db3291fd9cd80fc1b43fc
wagle04 <waglemanish28@gmail.com>
2018-09-27 19:25:12 +0545

    9/27/2018

--- ConsoleApp1/ConsoleApp1/sumof_arrays_using_classes.cs
+++ ConsoleApp1/ConsoleApp1/sumof_arrays_using_classes.cs
@@ -8,8 +8,16 @@ namespace ConsoleApp1
 {
     class sumof_arrays_using_classes
     {
+        public static int m, n;
         static void Main(string[] args)
         {
+            int a, b;
+            Console.WriteLine("Enter the size of matrices:");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            m = a;
+            n = b;
+
             forinput t = new forinput();
             int[,] first = t.input();
             int[,] second = t.input();
@@ -27,13 +35,16 @@ namespace ConsoleApp1
     class forinput
     {
         int[,] a;
+        int m = sumof_arrays_using_classes.m;
+        int n = sumof_arrays_using_classes.n;
         public int[,] input()
         {
-            a = new int[2, 2];
+
+            a = new int[m, n];
             Console.WriteLine("Enter matrix elements:");
-            for(int i = 0; i < 2; i++)
+            for (int i = 0; i < m; i++)
             {
-                for(int j = 0; j < 2; j++)
+                for (int j = 0; j < n; j++)
                 {
                     a[i, j] = Convert.ToInt32(Console.ReadLine());
                 }
@@ -42,9 +53,9 @@ namespace ConsoleApp1
         }
         public void display(int[,] a)
         {
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < m; i++)
             {
-                for (int j = 0; j < 2; j++)
+                for (int j = 0; j < n; j++)
                 {
                     Console.Write(a[i, j] + "\t");
                 }
@@ -53,10 +64,10 @@ namespace ConsoleApp1
         }
         public int[,] sum(int[,] first, int[,] second)
         {
-            int[,] sum = new int[2, 2];
-            for (int i = 0; i < 2; i++)
+            int[,] sum = new int[m, n];
+            for (int i = 0; i < m; i++)
             {
-                for (int j = 0; j < 2; j++)
+                for (int j = 0; j < n; j++)
                 {
                     sum[i, j] = first[i, j] + second[i, j];
                 }
