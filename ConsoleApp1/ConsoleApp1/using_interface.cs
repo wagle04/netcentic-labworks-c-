e60297bdd596f0c97270330e907d136a8ea59b77
wagle04 <waglemanish28@gmail.com>
2018-10-01 10:44:41 +0545

    10/1/2018 (interface class)

---
+++ ConsoleApp1/ConsoleApp1/using_interface.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//we cannot use multiple interface in c# but we can use inteeface to do so
+namespace ConsoleApp1
+{
+    class using_interface
+    {
+        static void Main(string[] args)
+        {
+            circle c = new circle();
+            rectangle r = new rectangle();
+            driver d = new driver();
+            d.calculate(c);
+            d.calculate(r);
+            Console.ReadLine();
+        }
+    }
+    interface shape
+    {
+        void area();
+    }
+    class circle : shape
+    {
+        public void area()
+        {
+            double p, r, a;
+            p = 3.14;
+            r = 4;
+            a = p * r * r;
+            Console.WriteLine(a);
+        }
+    }
+    class rectangle : shape
+    {
+        public void area()
+        {
+            int l, b, a;
+            l = 5;
+            b = 10;
+            a = l * b;
+            Console.WriteLine(a);
+
+        }
+    }
+    class driver
+    {
+        public void calculate(shape s)
+        {
+            s.area();
+        }
+    }
+}
