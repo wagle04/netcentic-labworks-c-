b85765f1f53d47b1fe2f1cc0d4c12e069cc68c49
wagle04 <waglemanish28@gmail.com>
2018-11-13 21:24:04 +0545

    datagrid view

---
+++ WebApplication1/WebApplication1/database grid view.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="database grid view.aspx.cs" Inherits="WebApplication1.database_grid_view" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="GridView1" runat="server" AllowPaging="True" CellPadding="10" OnPageIndexChanging="GridView1_PageIndexChanging" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" PageSize="5">
+            <PagerSettings Mode="NumericFirstLast" />
+            <PagerStyle BackColor="Pink" BorderColor="Red" BorderStyle="Dotted" BorderWidth="5px" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/database grid view.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class database_grid_view : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connstr = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+            SqlConnection con = new SqlConnection(connstr);
+            string sql = "select * from tblLogin"; //where username=@username AND password=@password";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            //cmd.Parameters.AddWithValue("@username", txtUsername.Text);
+            //cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            GridView1.DataBind();
+        }
+    }
+}
